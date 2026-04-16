eaf03abc08d77e9dea15cc634d490efaf42f0e0c
¨amadoch¨ <abec2003@hotmail.com>
2026-04-16 01:18:21 -0400

    Primera implementación del layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,44 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    },
-    
-    imageService: "cloudflare",
+  adapter: cloudflare({    
+    imageService: 'cloudflare',
   }),
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  fonts: [
+    {
+      name: 'Kumbh Sans',
+      provider: fontProviders.local(),
+      cssVariable: '--font-kumbh-sans',
+      fallbacks: [
+        'ui-sans-serif', 
+        'system-ui',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      options: {
+        variants: [
+          {
+            src: [
+              './src/assets/fonts/kumbh_sans.woff',
+              './src/assets/fonts/kumbh_sans.ttf'
+            ],
+            weight: '100 900',
+            style: 'normal'
+          }
+        ]
+      }
+    }
+  ]
 });
