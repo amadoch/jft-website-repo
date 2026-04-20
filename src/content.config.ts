ffb415ef332087e9e4cc474958f0e572e8762bec
¨amadoch¨ <abec2003@hotmail.com>
2026-04-20 17:47:46 -0400

    Conectado el contenido dinamico sobre endpoint comunicados/[slug], creado tambien endpoint estatico comunicados/ para mostrar colección de articulos

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,4 @@
-import { generateCollections } from "@sensinum/astro-strapi-loader";
+import { generateCollections } from "@sensinum/astro-strapi-loader"
 
 const strapiCollections = await generateCollections({
   url: import.meta.env.CMS_URL,
@@ -7,10 +7,67 @@ const strapiCollections = await generateCollections({
   {
     name: 'contacto',
     query: {
-      fields: ['horario_apertura', 'horario_cierre'],
+      fields: [
+        'horario_apertura',
+        'horario_cierre'
+      ],
       populate: {
         contactos: true
       }
+    }
+  },
+  {
+    name: 'comunicados',
+    collectionName: 'comunicado',
+    idGenerator(data) {
+      return data.slug as string
+    },
+    query: {
+      filters: {
+        categoria: {
+          $eq: 'comunicado'
+        }
+      },
+      pagination: {
+        start: 0,
+        limit: 10,
+      },
+    }
+  },
+  {
+    name: 'comunicados',
+    collectionName: 'aviso',
+    idGenerator(data) {
+      return data.slug as string
+    },
+    query: {
+      filters: {
+        categoria: {
+          $eq: 'aviso'
+        }
+      },
+      pagination: {
+        start: 0,
+        limit: 10,
+      },
+    }
+  },
+  {
+    name: 'comunicados',
+    collectionName: 'evento',
+    idGenerator(data) {
+      return data.slug as string
+    },
+    query: {
+      filters: {
+        categoria: {
+          $eq: 'evento'
+        }
+      },
+      pagination: {
+        start: 0,
+        limit: 10,
+      },
     }
   }
 ])
