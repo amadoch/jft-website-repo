29df01e52235fac41e811290148bd1f72e80aaa1
¨amadoch¨ <abec2003@hotmail.com>
2026-04-21 10:12:37 -0400

    Completado la carga de contenido dinamico sobre articulo navegado.

--- src/models/comunicado.model.ts
+++ src/models/comunicado.model.ts
@@ -4,9 +4,9 @@ export const comunicadoSchema = z.object({
   titulo: z.string(),
   slug: z.string(),
   resumen: z.string(),
-  contenido: z.array(z.object()),
+  contenido: z.any(),
   imagen_destacada: z.object({
-    alternativeText: z.nullable(z.string()),
+    alternativeText: z.string().nullable(),
     url: z.string()
   }),
   categoria: z.string()
